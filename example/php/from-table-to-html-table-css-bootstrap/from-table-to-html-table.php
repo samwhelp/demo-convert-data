8cf7fa73ac07cc1026203bfa484ae5e14143c1f0
samwhelp <samwhelp@users.noreply.github.com>
2016-10-13 17:26:28 +0800

    fix

--- example/php/from-table-to-html-table-css-bootstrap/from-table-to-html-table.php
+++ example/php/from-table-to-html-table-css-bootstrap/from-table-to-html-table.php
@@ -16,7 +16,7 @@
 	$rtn .= '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />' . PHP_EOL;
 
 	$rtn .= '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>' . PHP_EOL;
-    $rtn .= '<script src="js/bootstrap.min.js"></script>' . PHP_EOL;
+    $rtn .= '<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>' . PHP_EOL;
 
 	$rtn .= '<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->' . PHP_EOL;
 	$rtn .= '<!--[if lt IE 9]>' . PHP_EOL;
